01918e0c6f3d70c238cdaef9d3b9f03b624e9630
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2017-08-22 20:28:38 +0200

    Change the look

--- app/TalkWeb.hs
+++ app/TalkWeb.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE RecordWildCards #-}
 
+import           Control.Monad.IO.Class      (liftIO)
+import           Data.List
+import           Data.Monoid                 ((<>))
+import           Data.Text                   (Text)
+import qualified Data.Text                   as T
 import qualified Database.Persist.Postgresql as DB
-import Data.Monoid ((<>))
-import Web.Scotty.Trans
-import Control.Monad.IO.Class (liftIO)
-import Lucid
+import           Lucid
+import           Web.Scotty.Trans
 
 import Model
 import WebService
@@ -23,11 +25,22 @@ template content = do
     setHeader "Content-Type" "text/html"
     text . renderText . defaultTemplate $ content
 
-composeBox :: DB.Entity Compose -> Html ()
-composeBox (DB.Entity _ c) =
+composeBox :: Compose -> Html ()
+composeBox c =
     a_ [href_ (composeComposeId c), class_ " compose ", class_ (composeStatus c)] $ do
         span_ [class_ "composeId"] (toHtml $ composeComposeId c)
         span_ [class_ "status"] (toHtml $ composeStatus c)
+
+composeRow :: ([Text], [Compose]) -> Html ()
+composeRow (release:_, composes) = do
+    h2_ $ toHtml release
+    div_ [class_ "row"] $ mconcat $ map composeBox composes
+
+
+getRelease :: Compose -> Text
+getRelease Compose{..} =
+    let (prefix, date) = T.breakOnEnd "-" composeComposeId
+    in T.dropEnd 1 prefix
 
 composePage :: Compose -> Html ()
 composePage Compose{..} = do
@@ -37,12 +50,16 @@ composePage Compose{..} = do
         li_ $ "First heard of: " <> toHtml (fmtTime composeCreatedOn)
         li_ $ "Last heard of: " <> toHtml (fmtTime composeModifiedOn)
     h2_ "Comments"
+    p_ "To be done"
 
 main :: IO ()
 main = runService $ do
     get "/" $ do
-        composes <- runDB $ DB.selectList [] [DB.Desc ComposeId]
-        template $ mconcat $ map composeBox composes
+        composes <- runDB $ DB.selectList [] [DB.Desc ComposeComposeId]
+        template $ mconcat
+                 $ map (composeRow . unzip)
+                 $ groupBy (\x y -> fst x == fst y)
+                 $ map ((\x -> (getRelease x, x)) . DB.entityVal) composes
 
     get "/:id" $ do
         cid <- param "id"

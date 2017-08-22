f6fe8c46583cafa46b6f1c1d184152350fcfca25
Lubomír Sedlář <lubomir.sedlar@gmail.com>
2017-08-22 19:22:36 +0200

    Use lucid for generating HTML

--- app/TalkWeb.hs
+++ app/TalkWeb.hs
@@ -1,29 +1,55 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
 
 import qualified Database.Persist.Postgresql as DB
+import Data.Monoid ((<>))
 import Web.Scotty.Trans
 import Control.Monad.IO.Class (liftIO)
-import Text.Hamlet
-import Text.Blaze.Html.Renderer.Text
+import Lucid
 
 import Model
 import WebService
 
+defaultTemplate :: Html a -> Html a
+defaultTemplate content = doctypehtml_ $ do
+    head_ $ do
+        meta_ [charset_ "utf-8"]
+        link_ [href_ "/static/style.css", rel_ "stylesheet"]
+    body_ $ div_ [class_ "content"] content
+
+template :: Html a -> Action
+template content = do
+    setHeader "Content-Type" "text/html"
+    text . renderText . defaultTemplate $ content
+
+composeBox :: DB.Entity Compose -> Html ()
+composeBox (DB.Entity _ c) =
+    a_ [href_ (composeComposeId c), class_ " compose ", class_ (composeStatus c)] $ do
+        span_ [class_ "composeId"] (toHtml $ composeComposeId c)
+        span_ [class_ "status"] (toHtml $ composeStatus c)
+
+composePage :: Compose -> Html ()
+composePage Compose{..} = do
+    h1_ $ toHtml composeComposeId
+    ul_ $ do
+        li_ $ a_ [href_ composeLocation] "View"
+        li_ $ "First heard of: " <> toHtml (fmtTime composeCreatedOn)
+        li_ $ "Last heard of: " <> toHtml (fmtTime composeModifiedOn)
+    h2_ "Comments"
+
 main :: IO ()
 main = runService $ do
     get "/" $ do
-        setHeader "Content-Type" "text/html"
         composes <- runDB $ DB.selectList [] [DB.Desc ComposeId]
-        text $ renderHtml $ $(shamletFile "templates/index.hamlet")
+        template $ mconcat $ map composeBox composes
 
     get "/:id" $ do
         cid <- param "id"
-        setHeader "Content-Type" "text/html"
         mcompose <- runDB $ DB.selectList [ComposeComposeId DB.==. cid] []
         case mcompose of
-            [DB.Entity _ compose] -> text $ renderHtml $ $(shamletFile "templates/compose.hamlet")
-            _ -> text "404"
+            [DB.Entity _ compose] -> template $ composePage compose
+            _ -> next
 
     get "/static/style.css" $ do
         setHeader "Content-Type" "text/css"
